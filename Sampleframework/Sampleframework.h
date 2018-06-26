f057f7baa12224e976183b4e7370435f21764548
vijay <vdmasal@gmail.com>
2018-06-26 11:21:58 +0530

    Initial Commit

---
+++ Sampleframework/Sampleframework.h
@@ -0,0 +1,19 @@
+//
+//  Sampleframework.h
+//  Sampleframework
+//
+//  Created by Emberify_Vijay on 26/06/18.
+//  Copyright © 2018 Vijay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sampleframework.
+FOUNDATION_EXPORT double SampleframeworkVersionNumber;
+
+//! Project version string for Sampleframework.
+FOUNDATION_EXPORT const unsigned char SampleframeworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sampleframework/PublicHeader.h>
+
+
