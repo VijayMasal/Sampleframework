fc03c1a56bcd9b137c899c10cd7990d866fe2e06
vijay <vdmasal@gmail.com>
2018-06-26 11:29:40 +0530

    Add pod file

---
+++ Sampleframework/MasterClasss.h
@@ -0,0 +1,13 @@
+//
+//  MasterClasss.h
+//  Sampleframework
+//
+//  Created by Emberify_Vijay on 26/06/18.
+//  Copyright © 2018 Vijay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MasterClasss : NSObject
+
+@end
---
+++ Sampleframework/MasterClasss.m
@@ -0,0 +1,13 @@
+//
+//  MasterClasss.m
+//  Sampleframework
+//
+//  Created by Emberify_Vijay on 26/06/18.
+//  Copyright © 2018 Vijay. All rights reserved.
+//
+
+#import "MasterClasss.h"
+
+@implementation MasterClasss
+
+@end
